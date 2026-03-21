697a0e3d41a18adc42e39de7bb06fb6f3d69bb0d
azpun <azpunari@gmail.com>
2026-03-21 19:50:24 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Tasks Tracker</h1>
+        </div>
+        <div>
+          <nav></nav>
+        </div>
+      </header>
+      <main>
+        <div>
+          <h2>Tasks</h2>
+        </div>
+      </main>
+      <footer>
+        <div>
+          <h3>Footer</h3>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default App;
